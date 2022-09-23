037b1cedc67cdb53b90c20d5cff3e1dcf879fe1b
Admin <Admin@TEB123008>
2022-09-23 09:05:46 +0200

    Add project files.

---
+++ Kap 5 uppgift 10/Kap_5_uppgift_10.py
@@ -0,0 +1,9 @@
+print("\t Gradertabell")
+print("=============================")
+print("Celsius\t Farenhet")
+c=40
+while c>=-40:
+    f=32+c*9/5
+    print(c, " \t ", f)
+    c-=1
+print("=============================")
